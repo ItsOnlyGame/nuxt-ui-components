5b2b301ab2a7412f9d84c867916f2e12483c144b
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-14 20:38:14 +0200

    Updated buttons page

--- pages/buttons.vue
+++ pages/buttons.vue
@@ -1,39 +1,36 @@
 <template>
-    <div>
-      <div class="flex flex-col items-center gap-2 w-32">
-        <h2 class="text-slate-100">Solid buttons</h2>
-        <Button @click="onClick" size="xs">Some text (xs)</Button>
-        <Button @click="onClick" size="sm">Some text (sm)</Button>
-        <Button @click="onClick" size="md">Some text (md)</Button>
-        <Button @click="onClick" size="lg">Some text (lg)</Button>
-        <Button @click="onClick" size="xl">Some text (xl)</Button>
-      </div>
-  
-      <div class="flex flex-col items-center gap-2 w-32">
-        <h2 class="text-slate-100">Ghost buttons</h2>
-        <Button @click="onClick" size="xs" type="ghost">Some text (xs)</Button>
-        <Button @click="onClick" size="sm" type="ghost">Some text (sm)</Button>
-        <Button @click="onClick" size="md" type="ghost">Some text (md)</Button>
-        <Button @click="onClick" size="lg" type="ghost">Some text (lg)</Button>
-        <Button @click="onClick" size="xl" type="ghost">Some text (xl)</Button>
-      </div>
-  
-      <div class="flex flex-col items-center gap-2 w-32">
-        <h2 class="text-slate-100">Buttons</h2>
-        <Button @click="onClick" size="xs" type="outline">Some text (xs)</Button>
-        <Button @click="onClick" size="sm" type="outline">Some text (sm)</Button>
-        <Button @click="onClick" size="md" type="outline">Some text (md)</Button>
-        <Button @click="onClick" size="lg" type="outline">Some text (lg)</Button>
-        <Button @click="onClick" size="xl" type="outline">Some text (xl)</Button>
-      </div>
+  <div class="flex flex-row justify-evenly w-full flex-wrap gap-6">
+    <div class="flex flex-col items-center gap-2">
+      <Heading size="sm">Solid buttons</Heading>
+      <Button @click="onClick" size="xs">Some text (xs)</Button>
+      <Button @click="onClick" size="sm">Some text (sm)</Button>
+      <Button @click="onClick" size="md">Some text (md)</Button>
+      <Button @click="onClick" size="lg">Some text (lg)</Button>
+      <Button @click="onClick" size="xl">Some text (xl)</Button>
     </div>
-  </template>
-  
-  <script setup>
-  
-  const onClick = (e) => {
-    console.log('button works!')
-  }
-  
-  </script>
-  
+
+    <div class="flex flex-col items-center gap-2">
+      <Heading size="sm">Ghost buttons</Heading>
+      <Button @click="onClick" size="xs" variant="ghost">Some text (xs)</Button>
+      <Button @click="onClick" size="sm" variant="ghost">Some text (sm)</Button>
+      <Button @click="onClick" size="md" variant="ghost">Some text (md)</Button>
+      <Button @click="onClick" size="lg" variant="ghost">Some text (lg)</Button>
+      <Button @click="onClick" size="xl" variant="ghost">Some text (xl)</Button>
+    </div>
+
+    <div class="flex flex-col items-center gap-2">
+      <Heading size="sm">Outline Buttons</Heading>
+      <Button @click="onClick" size="xs" variant="outline">Some text (xs)</Button>
+      <Button @click="onClick" size="sm" variant="outline">Some text (sm)</Button>
+      <Button @click="onClick" size="md" variant="outline">Some text (md)</Button>
+      <Button @click="onClick" size="lg" variant="outline">Some text (lg)</Button>
+      <Button @click="onClick" size="xl" variant="outline">Some text (xl)</Button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const onClick = (e) => {
+  console.log("button works!");
+};
+</script>

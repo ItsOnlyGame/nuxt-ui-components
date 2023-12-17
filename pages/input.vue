a60ea6f330294e2a050da5df1b2c144036224ddb
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-17 20:44:39 +0200

    Changed Input page

--- pages/input.vue
+++ pages/input.vue
@@ -1,33 +1,27 @@
 <template>
   <div class="flex flex-col gap-12">
+    <Heading size="sm">Default Text Input</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
-        <Input label="Email" helper="This would give help" size="sm" variant="default" />
-        <Input label="Email" helper="This would give help" size="md" variant="default" />
-        <Input label="Email" helper="This would give help" size="lg" variant="default" />
+        <Input label="Text" helper="This would give help" size="sm" v-model="text" variant="default" />
+        <Input label="Text" helper="This would give help" size="md" v-model="text" variant="default" />
+        <Input label="Text" helper="This would give help" size="lg" v-model="text" variant="default" />
       </div>
 
       <div class="flex flex-col gap-4">
-        <Input label="Email" helper="This would give help" size="sm" variant="default" disabled />
-        <Input label="Email" helper="This would give help" size="md" variant="default" disabled />
-        <Input label="Email" helper="This would give help" size="lg" variant="default" disabled />
+        <Input label="Text" helper="This would give help" size="sm" variant="default" v-model="text" disabled />
+        <Input label="Text" helper="This would give help" size="md" variant="default" v-model="text" disabled />
+        <Input label="Text" helper="This would give help" size="lg" variant="default" v-model="text" disabled />
       </div>
     </div>
 
+    <Heading size="sm">Invalid Text Input</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
-        <form @submit="() => console.log('Done!')" class="flex flex-col gap-4">
-          <Input label="Email" helper="This would give help" size="sm" variant="default" required />
-          <Input label="Email" helper="This would give help" size="md" variant="default" required />
-          <Input label="Email" helper="This would give help" size="lg" variant="default" required />
-        </form>
-      </div>
-
-      <div class="flex flex-col gap-4">
-        <form @submit="() => console.log('Done!')" class="flex flex-col gap-4">
-          <Input label="Email" helper="This would give help" size="sm" variant="default" disabled />
-          <Input label="Email" helper="This would give help" size="md" variant="default" disabled />
-          <Input label="Email" helper="This would give help" size="lg" variant="default" disabled />
+        <form class="flex flex-col gap-4">
+          <Input label="Text" helper="This would give help" size="sm" variant="default" v-model="text" pattern="" />
+          <Input label="Text" helper="This would give help" size="md" variant="default" v-model="text" pattern="" />
+          <Input label="Text" helper="This would give help" size="lg" variant="default" v-model="text" pattern="" />
         </form>
       </div>
     </div>
@@ -39,4 +33,6 @@ definePageMeta({
   title: 'Component: Input',
   description: 'Nuxt UI input component',
 })
+
+const text = ref('Some text')
 </script>

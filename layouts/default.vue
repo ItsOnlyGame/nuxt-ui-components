e416dcfe56055f194dc422dec4b480b6fc533a9e
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 12:10:54 +0200

    Using Header site component in default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,6 @@
 <template>
   <div>
-    <header class="flex flex-row justify-between gap-2 border-b border-primary-800 px-8 py-3">
-      <NuxtLink to="/">
-        <Heading>Nuxt UI Components</Heading>
-      </NuxtLink>
-      <div>
-        <NuxtLink to="/buttons">
-          <Button variant="ghost"> Buttons </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/headings">
-          <Button variant="ghost"> Headings </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/input">
-          <Button variant="ghost"> Input </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/checkbox">
-          <Button variant="ghost"> Checkbox </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/textarea">
-          <Button variant="ghost"> Textarea </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/modals">
-          <Button variant="ghost"> Modals </Button>
-        </NuxtLink>
-
-        <NuxtLink to="/slideover">
-          <Button variant="ghost"> Slideover </Button>
-        </NuxtLink>
-      </div>
-    </header>
+    <Header />
 
     <div class="flex h-full flex-col items-center p-4">
       <div class="flex h-full w-full max-w-7xl flex-col gap-8">

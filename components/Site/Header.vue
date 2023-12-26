3989fd8b3ffbb8fe8d41eb8a7b402b97f72cf5d8
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-12-26 12:10:40 +0200

    Moved UI components to its own folder seperate from site components

---
+++ components/Site/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="flex flex-row justify-between gap-2 border-b border-primary-800 px-8 py-3">
+    <NuxtLink to="/">
+      <Heading>Nuxt UI Components</Heading>
+    </NuxtLink>
+    <div>
+      <NuxtLink to="/buttons">
+        <Button variant="ghost"> Buttons </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/headings">
+        <Button variant="ghost"> Headings </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/input">
+        <Button variant="ghost"> Input </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/checkbox">
+        <Button variant="ghost"> Checkbox </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/textarea">
+        <Button variant="ghost"> Textarea </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/modals">
+        <Button variant="ghost"> Modals </Button>
+      </NuxtLink>
+
+      <NuxtLink to="/slideover">
+        <Button variant="ghost"> Slideover </Button>
+      </NuxtLink>
+    </div>
+  </header>
+</template>

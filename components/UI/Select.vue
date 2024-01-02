c91a0668a88188b0f6be55df5b103a3c921a73d1
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-02 18:38:35 +0200

    Select svg icon positions and disabled color fixed

--- components/UI/Select.vue
+++ components/UI/Select.vue
@@ -37,7 +37,7 @@ const selectTV = tv({
     base: 'relative my-6 md:w-60',
     select: 'peer relative w-full appearance-none outline-none transition-all disabled:cursor-not-allowed',
     label: 'absolute z-[1] transition-all',
-    svg: 'pointer-events-none absolute right-2 top-3.5 h-5 w-5 transition-all peer-disabled:cursor-not-allowed'
+    svg: 'pointer-events-none absolute transition-all peer-disabled:cursor-not-allowed'
   },
   variants: {
     variant: {
@@ -46,21 +46,24 @@ const selectTV = tv({
           'bg-body-light text-primary-500 peer-invalid:text-red-500 peer-focus:text-primary-800 peer-disabled:text-stone-400',
         select:
           'rounded border-2 border-primary-500 text-primary-950 placeholder-transparent invalid:border-red-500 focus:border-primary-700 disabled:border-stone-400 disabled:bg-transparent',
-        svg: 'text-primary-400 peer-focus:text-primary-800'
+        svg: 'text-primary-500 peer-focus:text-primary-800 peer-disabled:text-stone-400'
       }
     },
     size: {
       sm: {
         label: '-top-2 left-3.5 px-1.5 text-xs',
-        select: 'h-10 px-4'
+        select: 'h-10 px-4',
+        svg: 'right-2 top-3 h-5 w-5'
       },
       md: {
         label: '-top-2 left-3 px-1.5 text-sm',
-        select: 'h-12 px-4'
+        select: 'h-12 px-4',
+        svg: 'right-2 top-3.5 h-5 w-5'
       },
       lg: {
         label: 'text-md -top-3 left-3.5 px-2',
-        select: 'h-14 px-5 text-lg'
+        select: 'h-14 px-5 text-lg',
+        svg: 'right-2 top-4 h-6 w-6'
       }
     }
   },

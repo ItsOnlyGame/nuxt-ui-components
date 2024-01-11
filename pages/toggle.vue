b6a6915a3474188000f0e0fee3b8741a439b9e33
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-11 10:38:06 +0200

    Created a UIContainer component for the site

--- pages/toggle.vue
+++ pages/toggle.vue
@@ -1,35 +1,33 @@
 <template>
   <div class="flex flex-col gap-12">
     <div>
-      <span>
-        Currently {{ toggeledCount }} toggles are checked!
-      </span>
+      <span> Currently {{ toggeledCount }} toggles are checked! </span>
     </div>
 
     <Heading size="sm">Default Checkbox</Heading>
-    <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
-      <div class="flex flex-col gap-4">
-        <Toggle label="Text" helper="This would give help" size="sm" v-model="toggles[0]" />
-        <Toggle label="Text" helper="This would give help" size="md" v-model="toggles[1]" />
-        <Toggle label="Text" helper="This would give help" size="lg" v-model="toggles[2]" />
-      </div>
+    <div class="flex w-full flex-row flex-wrap justify-between gap-6">
+      <UIContainer class="w-max">
+        <Toggle label="Text" helper="Toggle can also have helper text" size="sm" v-model="toggles[0]" />
+        <Toggle label="Text" helper="Toggle can also have helper text" size="md" v-model="toggles[1]" />
+        <Toggle label="Text" helper="Toggle can also have helper text" size="lg" v-model="toggles[2]" />
+      </UIContainer>
 
-      <div class="flex flex-col gap-4">
-        <Toggle label="Text" helper="This would give help" size="sm" v-model="toggles[3]" disabled />
-        <Toggle label="Text" helper="This would give help" size="md" v-model="toggles[4]" disabled />
-        <Toggle label="Text" helper="This would give help" size="lg" v-model="toggles[5]" disabled />
-      </div>
+      <UIContainer class="w-max">
+        <Toggle label="Text" helper="Toggle can also have helper text" size="sm" v-model="toggles[3]" disabled />
+        <Toggle label="Text" helper="Toggle can also have helper text" size="md" v-model="toggles[4]" disabled />
+        <Toggle label="Text" helper="Toggle can also have helper text" size="lg" v-model="toggles[5]" disabled />
+      </UIContainer>
     </div>
 
     <Heading size="sm">Invalid Checkbox</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
-      <div class="flex flex-col gap-4">
+      <UIContainer class="w-max">
         <form class="flex flex-col gap-4">
-          <Toggle label="Text" helper="This would give help" size="sm" v-model="toggles[6]" pattern="" />
-          <Toggle label="Text" helper="This would give help" size="md" v-model="toggles[7]" pattern="" />
-          <Toggle label="Text" helper="This would give help" size="lg" v-model="toggles[8]" pattern="" />
+          <Toggle label="Text" helper="Toggle can also have helper text" size="sm" v-model="toggles[6]" pattern="" />
+          <Toggle label="Text" helper="Toggle can also have helper text" size="md" v-model="toggles[7]" pattern="" />
+          <Toggle label="Text" helper="Toggle can also have helper text" size="lg" v-model="toggles[8]" pattern="" />
         </form>
-      </div>
+      </UIContainer>
     </div>
   </div>
 </template>
@@ -37,13 +35,13 @@
 <script setup>
 definePageMeta({
   title: 'Component: Toggle',
-  description: 'Nuxt UI toggle component',
+  description: 'Nuxt UI toggle component'
 })
 
-const toggles = ref([]) 
+const toggles = ref([])
 
 const toggeledCount = computed(() => {
-  const toggeled = toggles.value.filter(value => !!value)
+  const toggeled = toggles.value.filter((value) => !!value)
   return toggeled.length
 })
 </script>

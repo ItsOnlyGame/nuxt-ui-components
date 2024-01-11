eb5f3fd21c0347fce68318d864229555f3a86164
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-11 10:39:57 +0200

    Updated small text values of multiple components on the site

--- pages/checkbox.vue
+++ pages/checkbox.vue
@@ -9,15 +9,15 @@
     <Heading size="sm">Default Checkbox</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
-        <Checkbox label="Text" helper="This would give help" size="sm" v-model="checkboxes[0]" />
-        <Checkbox label="Text" helper="This would give help" size="md" v-model="checkboxes[1]" />
-        <Checkbox label="Text" helper="This would give help" size="lg" v-model="checkboxes[2]" />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="sm" v-model="checkboxes[0]" />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="md" v-model="checkboxes[1]" />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="lg" v-model="checkboxes[2]" />
       </div>
 
       <div class="flex flex-col gap-4">
-        <Checkbox label="Text" helper="This would give help" size="sm" v-model="checkboxes[3]" disabled />
-        <Checkbox label="Text" helper="This would give help" size="md" v-model="checkboxes[4]" disabled />
-        <Checkbox label="Text" helper="This would give help" size="lg" v-model="checkboxes[5]" disabled />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="sm" v-model="checkboxes[3]" disabled />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="md" v-model="checkboxes[4]" disabled />
+        <Checkbox label="Text" helper="Checkbox can also have helper text" size="lg" v-model="checkboxes[5]" disabled />
       </div>
     </div>
 
@@ -25,9 +25,9 @@
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
         <form class="flex flex-col gap-4">
-          <Checkbox label="Text" helper="This would give help" size="sm" v-model="checkboxes[6]" pattern="" />
-          <Checkbox label="Text" helper="This would give help" size="md" v-model="checkboxes[7]" pattern="" />
-          <Checkbox label="Text" helper="This would give help" size="lg" v-model="checkboxes[8]" pattern="" />
+          <Checkbox label="Text" helper="Checkbox can also have helper text" size="sm" v-model="checkboxes[6]" pattern="" />
+          <Checkbox label="Text" helper="Checkbox can also have helper text" size="md" v-model="checkboxes[7]" pattern="" />
+          <Checkbox label="Text" helper="Checkbox can also have helper text" size="lg" v-model="checkboxes[8]" pattern="" />
         </form>
       </div>
     </div>
--- pages/input.vue
+++ pages/input.vue
@@ -3,15 +3,15 @@
     <Heading size="sm">Default Text Input</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
-        <Input label="Text" helper="This would give help" size="sm" v-model="text" variant="default" />
-        <Input label="Text" helper="This would give help" size="md" v-model="text" variant="default" />
-        <Input label="Text" helper="This would give help" size="lg" v-model="text" variant="default" />
+        <Input label="Text" helper="Input can also have helper text" size="sm" v-model="text" variant="default" />
+        <Input label="Text" helper="Input can also have helper text" size="md" v-model="text" variant="default" />
+        <Input label="Text" helper="Input can also have helper text" size="lg" v-model="text" variant="default" />
       </div>
 
       <div class="flex flex-col gap-4">
-        <Input label="Text" helper="This would give help" size="sm" variant="default" v-model="text" disabled />
-        <Input label="Text" helper="This would give help" size="md" variant="default" v-model="text" disabled />
-        <Input label="Text" helper="This would give help" size="lg" variant="default" v-model="text" disabled />
+        <Input label="Text" helper="Input can also have helper text" size="sm" variant="default" v-model="text" disabled />
+        <Input label="Text" helper="Input can also have helper text" size="md" variant="default" v-model="text" disabled />
+        <Input label="Text" helper="Input can also have helper text" size="lg" variant="default" v-model="text" disabled />
       </div>
     </div>
 
@@ -19,9 +19,9 @@
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
         <form class="flex flex-col gap-4">
-          <Input label="Text" helper="This would give help" size="sm" variant="default" v-model="text" pattern="" />
-          <Input label="Text" helper="This would give help" size="md" variant="default" v-model="text" pattern="" />
-          <Input label="Text" helper="This would give help" size="lg" variant="default" v-model="text" pattern="" />
+          <Input label="Text" helper="Input can also have helper text" size="sm" variant="default" v-model="text" pattern="" />
+          <Input label="Text" helper="Input can also have helper text" size="md" variant="default" v-model="text" pattern="" />
+          <Input label="Text" helper="Input can also have helper text" size="lg" variant="default" v-model="text" pattern="" />
         </form>
       </div>
     </div>

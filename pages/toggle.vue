0a6fd04e847225ab4b98e6f98d2a9a115552b173
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-11 10:43:03 +0200

    Removed example of invalid Toggle

--- pages/toggle.vue
+++ pages/toggle.vue
@@ -18,17 +18,6 @@
         <Toggle label="Text" helper="Toggle can also have helper text" size="lg" v-model="toggles[5]" disabled />
       </UIContainer>
     </div>
-
-    <Heading size="sm">Invalid Checkbox</Heading>
-    <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
-      <UIContainer class="w-max">
-        <form class="flex flex-col gap-4">
-          <Toggle label="Text" helper="Toggle can also have helper text" size="sm" v-model="toggles[6]" pattern="" />
-          <Toggle label="Text" helper="Toggle can also have helper text" size="md" v-model="toggles[7]" pattern="" />
-          <Toggle label="Text" helper="Toggle can also have helper text" size="lg" v-model="toggles[8]" pattern="" />
-        </form>
-      </UIContainer>
-    </div>
   </div>
 </template>
 

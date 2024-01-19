9ad06639fa2d8701ecc6c9fef05dea3c3eb92e45
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-19 20:49:14 +0200

    Fixed text in time-picker page

--- pages/UI/time-picker.vue
+++ pages/UI/time-picker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col gap-12">
     <Heading size="sm">Default TimePicker</Heading>
-    <span> Currently date is {{ time }} </span>
+    <span> Currently time is {{ time }} </span>
 
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-row gap-4">

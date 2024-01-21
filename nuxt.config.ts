bd22936cfbac262c0a683aba1c2b4cd28d90a761
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-21 15:04:09 +0200

    Add Geist font styles and make it default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  css: ['./assets/index.css', './assets/tailwind.css'],
+  css: ['./assets/index.css', './assets/tailwind.css', './assets/fonts/geist.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@hypernym/nuxt-anime'],
   typescript: {
     typeCheck: true

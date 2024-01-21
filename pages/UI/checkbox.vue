1b998fc6ade7886463d00bfcf5b7fe69db015276
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2024-01-21 17:19:21 +0200

    Refactor site pages: Added IDs to toggles, checkboxes, and inputs

--- pages/UI/checkbox.vue
+++ pages/UI/checkbox.vue
@@ -1,23 +1,60 @@
 <template>
   <div class="flex flex-col gap-12">
     <div>
-      <span>
-        Currently {{ checkedCount }} checkboxes are checked!
-      </span>
+      <span> Currently {{ checkedCount }} checkboxes are checked! </span>
     </div>
 
     <Heading size="sm">Default Checkbox</Heading>
     <div class="flex w-full flex-row flex-wrap justify-evenly gap-6">
       <div class="flex flex-col gap-4">
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="sm" v-model="checkboxes[0]" />
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="md" v-model="checkboxes[1]" />
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="lg" v-model="checkboxes[2]" />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="sm"
+          v-model="checkboxes[0]"
+          id="checkbox-01"
+        />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="md"
+          v-model="checkboxes[1]"
+          id="checkbox-02"
+        />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="lg"
+          v-model="checkboxes[2]"
+          id="checkbox-03"
+        />
       </div>
 
       <div class="flex flex-col gap-4">
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="sm" v-model="checkboxes[3]" disabled />
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="md" v-model="checkboxes[4]" disabled />
-        <Checkbox label="Text" helper="Checkbox can also have helper text" size="lg" v-model="checkboxes[5]" disabled />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="sm"
+          v-model="checkboxes[3]"
+          disabled
+          id="checkbox-04"
+        />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="md"
+          v-model="checkboxes[4]"
+          disabled
+          id="checkbox-05"
+        />
+        <Checkbox
+          label="Checkbox"
+          helper="Checkbox can also have helper text"
+          size="lg"
+          v-model="checkboxes[5]"
+          disabled
+          id="checkbox-06"
+        />
       </div>
     </div>
   </div>
@@ -26,13 +63,13 @@
 <script setup>
 definePageMeta({
   title: 'Component: Checkbox',
-  description: 'Nuxt UI checkbox component',
+  description: 'Nuxt UI checkbox component'
 })
 
-const checkboxes = ref([]) 
+const checkboxes = ref([])
 
 const checkedCount = computed(() => {
-  const checked = checkboxes.value.filter(value => !!value)
+  const checked = checkboxes.value.filter((value) => !!value)
   return checked.length
 })
 </script>
